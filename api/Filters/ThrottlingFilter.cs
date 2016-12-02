e00a43dcd5c3fd3ee16c7981992c4215eda1bdac
Tim Barton <tim@vouchercloud.com>
2016-12-02 16:16:19 +0000

    Added more throttling functionality

--- api/Filters/ThrottlingFilter.cs
+++ api/Filters/ThrottlingFilter.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Net.Http;
 
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Server.Kestrel;
 
 namespace api.Filters
 {
@@ -32,7 +34,7 @@ namespace api.Filters
         private readonly IDateTimeProvider _dateTimeProvider;
         private const int _throttleWindowSeconds = 10;
         private const int _throttleCount = 10;
-        private ConcurrentDictionary<string, int> _hostCounts = new ConcurrentDictionary<string, int>();
+        private readonly ConcurrentDictionary<string, ThrottleState> _hostCounts = new ConcurrentDictionary<string, ThrottleState>();
 
         public Throttler(IDateTimeProvider dateTimeProvider)
         {
@@ -41,7 +43,36 @@ namespace api.Filters
 
         public void Throttle(string requestHost)
         {
-            
+            _hostCounts.AddOrUpdate(
+                requestHost,
+                s => new ThrottleState(_dateTimeProvider.Now),
+                (s, state) =>
+                {
+                    if (state.ResetTime <= _dateTimeProvider.Now)
+                    {
+                        return new ThrottleState(_dateTimeProvider.Now);
+                    }
+
+                    if (state.Counter >= _throttleCount)
+                    {
+                        throw new HttpRequestException();
+                    }
+
+                    state.Counter++;
+                    return state;
+                });
+        }
+
+        private class ThrottleState
+        {
+            public ThrottleState(DateTime resetTime)
+            {
+                ResetTime = resetTime;
+            }
+
+            public int Counter { get; set; }
+
+            public DateTime ResetTime { get; private set; }
         }
     }
 
