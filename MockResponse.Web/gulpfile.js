23a554314c31c3cf4a3d838059dbf9f6f6641cb4
Tim Barton <tim@vouchercloud.com>
2017-11-20 12:47:15 +0000

    Integrated gulp to VS and updated some styles

--- MockResponse.Web/gulpfile.js
+++ MockResponse.Web/gulpfile.js
@@ -1,10 +1,12 @@
-﻿var gulp = require('gulp');
+﻿/// <binding ProjectOpened='default' />
+
+var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('default', function() {
   console.log("Running gulp");
 
-  return gulp.src('Content/Styles/styles.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('wwwroot/Dist'))
+    return gulp.src('Content/Styles/styles.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('wwwroot/Dist'));
 });
